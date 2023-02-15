1b55d952327212c0ef408801219eec9bd0faa56c
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-02-14 23:57:15 -0500

    additions to portfolioComponent.js

--- portfolio/src/components/PortfolioComponent/PortfolioComponent.js
+++ portfolio/src/components/PortfolioComponent/PortfolioComponent.js
@@ -1,4 +1,50 @@
 
+// Portfolio.jsx
+
+import React from "react";
+import "./PortfolioComponent.module.css";
+
+const projects = [
+    {
+        id: 1,
+        name: "BootyCamp",
+        description: "Description of Project 1",
+        image: "https://via.placeholder.com/300x200",
+        link: "https://www.project1.com"
+    },
+    {
+        id: 2,
+        name: "Project 2",
+        description: "Description of Project 2",
+        image: "https://via.placeholder.com/300x200",
+        link: "https://www.project2.com"
+    },
+
+];
+
+function PortfolioComponent() {
+    return (
+        <div className="portfolio">
+            {projects.map((project) => (
+                <a
+                    key={project.id}
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="project-card"
+                >
+                    <img src={project.image} alt={project.name} />
+                    <div className="overlay">
+                        <h3>{project.name}</h3>
+                        <p>{project.description}</p>
+                    </div>
+                </a>
+            ))}
+        </div>
+    );
+}
+
+export default PortfolioComponent;
 
 
 
